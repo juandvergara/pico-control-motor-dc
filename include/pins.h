e14ff0145d38a01ea9930f72b6f9ab8d3b221c8b
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-09-07 10:57:03 -0500

    Remap pins

--- include/pins.h
+++ include/pins.h
@@ -5,13 +5,12 @@
 #ifndef PICO_REMOTE_CAR_PINS_H
 #define PICO_REMOTE_CAR_PINS_H
 
-#define LED_PIN 17
 #define M1_PWM_PIN 7
 #define M1_ENA_PIN 8
 #define M1_ENB_PIN 9
 
-#define M1_ENC_A_PIN 20
-#define M1_ENC_B_PIN 21
+#define M1_ENC_A_PIN 21
+#define M1_ENC_B_PIN 20
 
 #define M2_PWM_PIN 10
 #define M2_ENA_PIN 11
@@ -20,7 +19,14 @@
 #define M2_ENC_A_PIN 19
 #define M2_ENC_B_PIN 18
 
- #define M1_ENC_INVERTED false                                                   
- #define M2_ENC_INVERTED true                                                              
+#define M3_PWM_PIN 13
+#define M3_ENA_PIN 14
+#define M3_ENB_PIN 15
+
+#define M3_ENC_A_PIN 17
+#define M3_ENC_B_PIN 16
+
+#define M1_ENC_INVERTED false                                                   
+#define M2_ENC_INVERTED true                                                              
 
 #endif //PICO_REMOTE_CAR_PINS_H
