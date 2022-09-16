f93a0e4af1ace7b625ed7ad9e93e1abc81481bf0
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-09-16 15:12:19 -0500

    Fix PID nad other things

--- pid_motor_test/pid_motor_test.cpp
+++ pid_motor_test/pid_motor_test.cpp
@@ -9,9 +9,9 @@ DCMotor motor1(M1_ENA_PIN, M1_ENB_PIN, M1_PWM_PIN);
 DCMotor motor2(M2_ENA_PIN, M2_ENB_PIN, M2_PWM_PIN);
 DCMotor motor3(M3_ENA_PIN, M3_ENB_PIN, M3_PWM_PIN);
 
-float kp = 60.0;
-float kd = 5.0;
-float ki = 15.0;
+float kp = 1.5;
+float kd = 0.3;
+float ki = 0.7;
 
 float joint_input, joint_effort, joint_setpoint = 0.0;
 float joint_position;
@@ -38,14 +38,12 @@ void initRobot()
     pid_rate = float(sample_time_ms) / 1000.0f;
 }
 
-void updatePid(int32_t joint_encoder_ticks)
+void updatePid(int32_t joint1_encoder_ticks)
 {
-    int32_t joint_ticks = joint_encoder_ticks;
-    float motor_vel = 0;
+    int32_t joint1_ticks = joint1_encoder_ticks;
+    float motor1_vel = 0;
 
-    joint_position = float(joint_ticks) * 360.0f / (80.0f * 127.7f * 4.0f);
-
-    // joint_setpoint = float(user_setpoint);
+    joint_position = float(joint1_ticks) * 360.0f / (80.0f * 127.7f * 4.0f);
 
     joint_input = joint_position;
 
@@ -53,26 +51,37 @@ void updatePid(int32_t joint_encoder_ticks)
 
     if (joint_effort < 0)
     {
-        motor_vel = (-(abs(joint_effort) + 90) / 180) * 0.60 - 0.4;
+        motor1_vel = ((joint_effort - 90) / 180);
     }
     else
     {
-        motor_vel = ((joint_effort + 90) / 180) * 0.6 + 0.4;
+        motor1_vel = ((joint_effort + 90) / 180);
     }
 
-    motor1.write(-motor_vel);
+    motor1.write(-motor1_vel);
+    // printf("Motor entregado %.2f \n", motor_vel);
+}
+
+bool timerCallback(repeating_timer_t *rt)
+{
+    updatePid(int32_t(PosEncoderM1));
+    return true;
 }
 
 int main()
 {
     stdio_init_all();
-    printf("Encoder Test");
+    printf("PID Motor test");
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     initRobot();
-
     encoder_setup();
-    char user_setpoin;
+
+    repeating_timer_t timer;
+    if (!add_repeating_timer_ms(-sample_time_ms, timerCallback, NULL, &timer))
+    {
+        printf("Failure by not set timer!! \n");
+    }
 
     int input_char;
     int input_char_index;
@@ -80,18 +89,17 @@ int main()
     char *char_pt2;
     char *char_pt3;
 
-    float angular = 0.0;
-    float linear = 0.0;
+    float joint1_sp = 0.0;
+    float joint2_sp = 0.0;
 
     while (true)
     {
-        // printf("Introduce el setpoint en formato linear,angular/\n");
+        printf("Introduce el setpoint en formato joint1,joint2/\n");
 
         input_char = getchar_timeout_us(0); // Esperar la entrada del usuario
         // printf("%c \n", input_char);
         while (input_char != PICO_ERROR_TIMEOUT)
         {
-            // gpio_put(LED_PIN, true);
             // printf(" %c ", ch);
             gpio_put(PICO_DEFAULT_LED_PIN, 1);
             putchar(input_char);                        // Print user input in console
@@ -101,27 +109,21 @@ int main()
                 in_buffer[input_char_index] = 0; // end of string
                 printf("\nreceived: %s\n", in_buffer);
                 input_char_index = 0;
-                linear = strtof(in_buffer, &char_pt1);     // Conversion string (char) to float
-                angular = strtof(char_pt1 + 1, &char_pt2); // Add 1 to bring up the comma
-                printf("Primer parámetro %.1f y el segundo parámetro es %.1f \n", linear, angular);
+                joint1_sp = strtof(in_buffer, &char_pt1);    // Conversion string (char) to float
+                joint2_sp = strtof(char_pt1 + 1, &char_pt2); // Add 1 to bring up the comma
+                // printf("Position to joint1: %.2f and position to joint 2: %.2f \n", joint1_sp, joint2_sp);
                 break;
             }
-
+            joint_setpoint = joint1_sp;
             input_char = getchar_timeout_us(0);
             printf("\n Caracter recibido \n");
-            joint_setpoint = angular; //
-            // printf("%.1f", joint_setpoint);
         }
         // gpio_put(LED_PIN, false);
         // printf("Entradas recibidas");
-
-        // sleep_ms(100);
-        //  Pedir por serial a posicion que se quiere
-        updatePid(int32_t(PosEncoderM1));
-        printf("El esfuerzo es de %.3f\n", joint_effort / 1.0);
-        printf("La posición es de %.3f\n", joint_position);
-        printf("El setpoint es de %.3f\n \n", joint_setpoint);
-        // sleep_ms(100);
+        printf("Effort: %.3f\n", joint_effort);
+        printf("Position: %.3f\n", joint_position);
+        printf("Setpoint: %.3f\n \n", joint_setpoint);
+        sleep_ms(500);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
     }
 }
