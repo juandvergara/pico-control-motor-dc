ffc13c8da26a7939ff1ea1e7a707e6cb7a8a91b5
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-09-28 15:16:02 -0500

    Encoder class

--- include/encoder.h
+++ include/encoder.h
@@ -9,23 +9,17 @@
 #include "pins.h"
 
 // pins
+class Encoder
+{
+private:
+    uint _chA_pin;
+    uint _chB_pin;
+    unsigned char _old_pos, _new_pos;
+    
+public:
+    volatile long encoder_pos;
+    Encoder(uint chA_pin, uint chB_pin);
+    void readPosition();
+};
 
-
-#define PREV_MASK 0x1
-#define CURR_MASK 0x2
-#define INVALID_MASK 0x3
-
-extern const uint32_t encoder1_mask; //= (0x01 << M1_ENC_A_PIN) | (0x01 << M1_ENC_B_PIN);
-extern const uint32_t encoder2_mask; //= (0x01 << M2_ENC_A_PIN) | (0x01 << M2_ENC_B_PIN);
-
-extern volatile int32_t encoder1_ticks;
-extern volatile uint32_t encoder1_state;
-
-extern volatile int32_t encoder2_ticks;
-extern volatile uint32_t encoder2_state;
-
-void encoder_setup();
-void encoder_callback(uint gpio, uint32_t events);
-
-
-#endif //PICO_REMOTE_CAR_ENCODER_H
+#endif // PICO_REMOTE_CAR_ENCODER_H
