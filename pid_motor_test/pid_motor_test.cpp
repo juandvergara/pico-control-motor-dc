eb41ba0fce9d6d7592ff2c3c69a4fd487fd37805
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-09-30 11:50:57 -0500

    Move three motor at same time

--- pid_motor_test/pid_motor_test.cpp
+++ pid_motor_test/pid_motor_test.cpp
@@ -13,16 +13,17 @@ DCMotor motor1(M1_ENA_PIN, M1_ENB_PIN, M1_PWM_PIN);
 DCMotor motor2(M2_ENA_PIN, M2_ENB_PIN, M2_PWM_PIN);
 DCMotor motor3(M3_ENA_PIN, M3_ENB_PIN, M3_PWM_PIN);
 
-float kp1 = 3.0;
+float kp1 = 5.0;
 float kd1 = 0.6;
 float ki1 = 1.4;
-float kp2 = 10.0;
+float kp2 = 8.0;
 float kd2 = 0.5;
-float ki2 = 1.0;
+float ki2 = 3.0;
 
 float joint_input1, joint_effort1, joint_setpoint1 = 0.0;
-float joint_position1, joint_position2;
+float joint_position1, joint_position2, joint_position3;
 float joint_input2, joint_effort2, joint_setpoint2 = 0.0;
+float joint_input3, joint_effort3, joint_setpoint3 = 0.0;
 uint32_t sample_time_ms = 20;
 float pid_rate;
 char user_setpoint;
@@ -32,6 +33,7 @@ uint16_t char_idx = 0;
 
 PID PID_Joint1(&joint_input1, &joint_effort1, &joint_setpoint1, kp1, ki1, kd1, sample_time_ms);
 PID PID_Joint2(&joint_input2, &joint_effort2, &joint_setpoint2, kp2, ki2, kd2, sample_time_ms);
+PID PID_Joint3(&joint_input3, &joint_effort3, &joint_setpoint3, kp2, ki2, kd2, sample_time_ms);
 
 uint32_t millis()
 {
@@ -42,42 +44,52 @@ void initRobot()
 {
     motor1.write(0.0);
     motor2.write(0.0);
+    motor3.write(0.0);
     // PID PID_Joint1(&joint_input1, &joint_effort1, &joint_setpoint1, kp, ki, kd, sample_time_ms);
     PID_Joint1.set_output_limits(-90.0f, 90.0f);
     PID_Joint2.set_output_limits(-90.0f, 90.0f);
+    PID_Joint3.set_output_limits(-90.0f, 90.0f);
     joint_setpoint1 = 0;
     joint_setpoint2 = 0;
+    joint_setpoint3 = 0;
     pid_rate = float(sample_time_ms) / 1000.0f;
 }
 
-void updatePid(int32_t joint1_encoder_ticks, int32_t joint2_encoder_ticks)
+void updatePid(int32_t joint1_encoder_ticks, int32_t joint2_encoder_ticks, int32_t joint3_encoder_ticks)
 {
     int32_t joint1_ticks = joint1_encoder_ticks;
     int32_t joint2_ticks = joint2_encoder_ticks;
+    int32_t joint3_ticks = joint3_encoder_ticks;
 
     float motor1_vel = 0;
     float motor2_vel = 0;
+    float motor3_vel = 0;
 
     joint_position1 = float(joint1_ticks) * 360.0f / (80.0f * 127.7f * 4.0f);
-    joint_position2 = float(joint2_ticks) * 360.0f / (80.0f * 65.5f * 3.4f);
+    joint_position2 = float(joint2_ticks) * 360.0f * 23.0f / (80.0f * 65.5f * 32.0f);
+    joint_position3 = float(joint3_ticks) * 360.0f * 23.0f / (80.0f * 65.5f * 32.0f);
 
     joint_input1 = joint_position1;
     joint_input2 = joint_position2;
+    joint_input3 = joint_position3;
 
     PID_Joint1.compute();
     PID_Joint2.compute();
+    PID_Joint3.compute();
 
     motor1_vel = (joint_effort1 / 90);
     motor2_vel = (joint_effort2 / 90);
+    motor3_vel = (joint_effort3 / 90);
 
     motor1.write(-motor1_vel);
     motor2.write(motor2_vel);
+    motor3.write(-motor3_vel);
     // printf("Motor entregado %.2f \n", motor_vel);
 }
 
 bool timerCallback(repeating_timer_t *rt)
 {
-    updatePid(int32_t(encoder1.encoder_pos), int32_t(encoder2.encoder_pos));
+    updatePid(int32_t(encoder1.encoder_pos), int32_t(encoder2.encoder_pos), int32_t(encoder3.encoder_pos));
     return true;
 }
 
@@ -114,10 +126,11 @@ int main()
 
     float joint1_sp = 0.0;
     float joint2_sp = 0.0;
+    float joint3_sp = 0.0;
 
     while (true)
     {
-        printf("Introduce el setpoint en formato joint1,joint2/\n");
+        //printf("Introduce el setpoint en formato joint1,joint2/\n");
 
         input_char = getchar_timeout_us(0); // Esperar la entrada del usuario
         // printf("%c \n", input_char);
@@ -133,20 +146,23 @@ int main()
                 printf("\nreceived: %s\n", in_buffer);
                 input_char_index = 0;
                 joint1_sp = strtof(in_buffer, &char_pt1);    // Conversion string (char) to float
-                joint2_sp = strtof(char_pt1 + 1, &char_pt2); // Add 1 to bring up the comma
+                joint2_sp = strtof(char_pt1 + 1, &char_pt2);    // Conversion string (char) to float
+                joint3_sp = strtof(char_pt2 + 1, &char_pt3); // Add 1 to bring up the comma
                 // printf("Position to joint1: %.2f and position to joint 2: %.2f \n", joint1_sp, joint2_sp);
                 break;
             }
             joint_setpoint1 = joint1_sp;
             joint_setpoint2 = joint2_sp;
+            joint_setpoint3 = -joint3_sp;
             input_char = getchar_timeout_us(0);
             printf("\n Caracter recibido \n");
         }
         // gpio_put(LED_PIN, false);
         // printf("Entradas recibidas");
-        //printf("Effort: %.3f, %.3f \n", joint_effort1, joint_effort2);
+        // printf("Effort: %.3f, %.3f \n", joint_effort1, joint_effort2);
         printf("Position1: %.3f\n", joint_position1);
-        printf("Position2: %.3f\n \n", joint_position2);
+        printf("Position2: %.3f\n", joint_position2);
+        printf("Position3: %.3f\n \n", joint_position3);
         sleep_ms(500);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
     }
