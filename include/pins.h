b6828094b16905cd0a899d92ec133056bdbad888
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-10-04 17:02:32 -0500

    Motor DC with two pins

--- include/pins.h
+++ include/pins.h
@@ -5,21 +5,21 @@
 #ifndef PICO_REMOTE_CAR_PINS_H
 #define PICO_REMOTE_CAR_PINS_H
 
-#define M1_PWM_PIN 7
-#define M1_ENA_PIN 8
-#define M1_ENB_PIN 9
+//#define M1_PWM_PIN 7
+#define M1_ENA_PIN 10
+#define M1_ENB_PIN 11
 
 #define M1_ENC_A_PIN 21
 #define M1_ENC_B_PIN 20
 
-#define M2_PWM_PIN 10
-#define M2_ENA_PIN 11
-#define M2_ENB_PIN 12
+//#define M2_PWM_PIN 10
+#define M2_ENA_PIN 12
+#define M2_ENB_PIN 13
 
 #define M2_ENC_A_PIN 19
 #define M2_ENC_B_PIN 18
 
-#define M3_PWM_PIN 13
+//#define M3_PWM_PIN 13
 #define M3_ENA_PIN 14
 #define M3_ENB_PIN 15
 
@@ -27,6 +27,7 @@
 #define M3_ENC_B_PIN 16
 
 #define M1_ENC_INVERTED false                                                   
-#define M2_ENC_INVERTED true                                                              
+#define M2_ENC_INVERTED false                                                   
+#define M3_ENC_INVERTED true                                                              
 
 #endif //PICO_REMOTE_CAR_PINS_H
