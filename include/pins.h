1d49fe3e6fb14a0b11e9a3ff1fbfb2326b4de86f
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-10-20 21:28:35 -0500

    Update pins for master and slave controllers

--- include/pins.h
+++ include/pins.h
@@ -5,29 +5,51 @@
 #ifndef PICO_REMOTE_CAR_PINS_H
 #define PICO_REMOTE_CAR_PINS_H
 
-//#define M1_PWM_PIN 7
-#define M1_ENA_PIN 10
-#define M1_ENB_PIN 11
+// Group 1 to master controller
 
-#define M1_ENC_A_PIN 21
-#define M1_ENC_B_PIN 20
+#define M0_ENA_PIN 10
+#define M0_ENB_PIN 11
 
-//#define M2_PWM_PIN 10
-#define M2_ENA_PIN 12
-#define M2_ENB_PIN 13
+#define M0_ENC_A_PIN 21
+#define M0_ENC_B_PIN 20
 
-#define M2_ENC_A_PIN 19
-#define M2_ENC_B_PIN 18
+#define M1_ENA_PIN 12
+#define M1_ENB_PIN 13
 
-//#define M3_PWM_PIN 13
-#define M3_ENA_PIN 14
-#define M3_ENB_PIN 15
+#define M1_ENC_A_PIN 19
+#define M1_ENC_B_PIN 18
 
-#define M3_ENC_A_PIN 17
-#define M3_ENC_B_PIN 16
+#define M2_ENA_PIN 14
+#define M2_ENB_PIN 15
 
-#define M1_ENC_INVERTED false                                                   
-#define M2_ENC_INVERTED false                                                   
-#define M3_ENC_INVERTED true                                                              
+#define M2_ENC_A_PIN 17
+#define M2_ENC_B_PIN 16
+
+// Group 2 to slave controller
+
+#define M3_ENA_PIN 10
+#define M3_ENB_PIN 11
+
+#define M3_ENC_A_PIN 21
+#define M3_ENC_B_PIN 20
+
+#define M4_ENA_PIN 12
+#define M4_ENB_PIN 13
+
+#define M4_ENC_A_PIN 19
+#define M4_ENC_B_PIN 18
+
+#define M5_ENA_PIN 14
+#define M5_ENB_PIN 15
+
+#define M5_ENC_A_PIN 17
+#define M5_ENC_B_PIN 16
+
+#define M0_ENC_INVERTED false
+#define M1_ENC_INVERTED false
+#define M2_ENC_INVERTED false
+#define M3_ENC_INVERTED true
+#define M4_ENC_INVERTED true
+#define M5_ENC_INVERTED true
 
 #endif //PICO_REMOTE_CAR_PINS_H
