1e0fc949bf3da3cfeaa42eb25718621211cf2462
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-11-21 18:32:59 -0500

    Control position and speed to one motor TOD0:Fix

--- test/pid_motor_test/pid_motor_test.cpp
+++ test/pid_motor_test/pid_motor_test.cpp
@@ -8,24 +8,20 @@
 #define PI 3.14159265359f
 
 Encoder encoder3(M3_ENC_A_PIN, M3_ENC_B_PIN);
-Encoder encoder4(M4_ENC_A_PIN, M4_ENC_B_PIN);
-Encoder encoder5(M5_ENC_A_PIN, M5_ENC_B_PIN);
 
 DCMotor motor3(M3_ENA_PIN, M3_ENB_PIN);
-DCMotor motor4(M4_ENA_PIN, M4_ENB_PIN);
-DCMotor motor5(M5_ENA_PIN, M5_ENB_PIN);
 
-float kp1 = 2.0;
+float kp1 = 0.5;
 float kd1 = 1.0;
-float ki1 = 0.5;
-float kp2 = 0.75;
-float kd2 = 1.0;
-float ki2 = 0.15;
+float ki1 = 0.125;
 
 float joint_input1, joint_effort1, joint_setpoint1 = 0.0;
-float joint_position1, joint_position2, joint_position3;
-float joint_input2, joint_effort2, joint_setpoint2 = 0.0;
-float joint_input3, joint_effort3, joint_setpoint3 = 0.0;
+float joint_position1, past_joint_position1;
+float joint_velocity1;
+float speed_target = 0;
+
+float v1Filt = 0;
+float v1Prev = 0;
 
 uint32_t sample_time_ms = 10;
 float pid_rate;
@@ -36,8 +32,6 @@ uint16_t char_idx = 0;
 float theta = 1.0 / (2.0 * PI * 10.0);
 
 PID PID_Joint1(&joint_input1, &joint_effort1, &joint_setpoint1, kp1, ki1, kd1, sample_time_ms);
-PID PID_Joint2(&joint_input2, &joint_effort2, &joint_setpoint2, kp2, ki2, kd2, sample_time_ms);
-PID PID_Joint3(&joint_input3, &joint_effort3, &joint_setpoint3, kp2, ki2, kd2, sample_time_ms);
 
 uint32_t millis()
 {
@@ -47,61 +41,40 @@ uint32_t millis()
 void initRobot()
 {
     motor3.write(0.0);
-    motor4.write(0.0);
-    motor5.write(0.0);
-    // PID PID_Joint1(&joint_input1, &joint_effort1, &joint_setpoint1, kp, ki, kd, sample_time_ms);
     PID_Joint1.set_output_limits(-1.0f, 1.0f);
-    PID_Joint2.set_output_limits(-1.0f, 1.0f);
-    PID_Joint3.set_output_limits(-1.0f, 1.0f);
     joint_setpoint1 = 0;
-    joint_setpoint2 = 0;
-    joint_setpoint3 = 0;
     pid_rate = float(sample_time_ms) / 1000.0f;
 }
 
-void updatePid(int32_t joint1_encoder_ticks, int32_t joint2_encoder_ticks, int32_t joint3_encoder_ticks)
+void updatePid(int32_t joint1_encoder_ticks)
 {
     int32_t joint1_ticks = joint1_encoder_ticks;
-    int32_t joint2_ticks = joint2_encoder_ticks;
-    int32_t joint3_ticks = joint3_encoder_ticks;
 
     float motor1_vel = 0;
-    float motor2_vel = 0;
-    float motor3_vel = 0;
-
+    past_joint_position1 = joint_position1;
     joint_position1 = float(joint1_ticks) * 360.0f / (80.0f * 127.7f * 4.0f);
-    joint_position2 = float(joint2_ticks) * 360.0f * 23.0f / (80.0f * 65.5f * 32.0f);
-    joint_position3 = float(joint3_ticks) * 360.0f * 23.0f / (80.0f * 65.5f * 32.0f);
-
     joint_input1 = joint_position1;
-    joint_input2 = joint_position2;
-    joint_input3 = joint_position3;
+
+    joint_velocity1 = (joint_position1 - past_joint_position1) / pid_rate;
+    v1Filt = 0.854 * v1Filt + 0.0728 * joint_velocity1 + 0.0728 * v1Prev;
+    v1Prev = joint_velocity1;
 
     PID_Joint1.compute();
-    PID_Joint2.compute();
-    PID_Joint3.compute();
 
-    motor1_vel = joint_effort1;
-    motor2_vel = joint_effort2;
-    motor3_vel = joint_effort3;
+    motor1_vel = joint_effort1 + (speed_target - v1Filt) * 0.15;
 
     motor3.write(-motor1_vel);
-    motor4.write(motor2_vel);
-    motor5.write(-motor3_vel);
-    // printf("Motor entregado %.2f \n", motor_vel);
 }
 
 bool timerCallback(repeating_timer_t *rt)
 {
-    updatePid(int32_t(encoder3.encoder_pos), int32_t(encoder4.encoder_pos), int32_t(encoder5.encoder_pos));
+    updatePid(int32_t(encoder3.encoder_pos));
     return true;
 }
 
 void encoders_callback(uint gpio, uint32_t events)
 {
     encoder3.readPosition();
-    encoder4.readPosition();
-    encoder5.readPosition();
 }
 
 int main()
@@ -113,8 +86,6 @@ int main()
     initRobot();
 
     gpio_set_irq_enabled_with_callback(M3_ENC_A_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &encoders_callback);
-    gpio_set_irq_enabled_with_callback(M4_ENC_A_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &encoders_callback);
-    gpio_set_irq_enabled_with_callback(M5_ENC_A_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &encoders_callback);
 
     repeating_timer_t timer;
     if (!add_repeating_timer_ms(-sample_time_ms, timerCallback, NULL, &timer))
@@ -122,55 +93,60 @@ int main()
         printf("Failure by not set timer!! \n");
     }
 
+    float theta0 = 0;
+    float dot_theta0 = 0;
+    float thetaf = 90;
+    float dot_thetaf = 0;
+    float t_final = 3;
+
+    float a = theta0;
+    float b = dot_theta0;
+    float c = 3.0 * (thetaf - theta0) / pow(t_final, 2) - 2 * dot_theta0 / pow(t_final, 2) - dot_thetaf / pow(t_final, 2);
+    float d = -2.0 * (thetaf - theta0) / pow(t_final, 3) + (dot_thetaf - dot_theta0) / pow(t_final, 2);
+
     int input_char;
     int input_char_index;
     char *char_pt1;
-    char *char_pt2;
-    char *char_pt3;
 
     float joint1_sp = 0.0;
-    float joint2_sp = 0.0;
-    float joint3_sp = 0.0;
+
+    sleep_ms(3000);
 
     while (true)
     {
-        // printf("Introduce el setpoint en formato joint1,joint2/\n");
-
-        input_char = getchar_timeout_us(0); // Esperar la entrada del usuario
-        // printf("%c \n", input_char);
+        input_char = getchar_timeout_us(0);
         while (input_char != PICO_ERROR_TIMEOUT)
         {
-            // printf(" %c ", ch);
             gpio_put(PICO_DEFAULT_LED_PIN, 1);
             putchar(input_char);                        // Print user input in console
             in_buffer[input_char_index++] = input_char; // Index user input to buffer array
             if (input_char == '/')
             {
-                in_buffer[input_char_index] = 0; // end of string
-                //printf("\nreceived: %s\n", in_buffer);
+                in_buffer[input_char_index] = 0;
                 input_char_index = 0;
-                joint1_sp = strtof(in_buffer, &char_pt1);    // Conversion string (char) to float
-                joint2_sp = strtof(char_pt1 + 1, &char_pt2); // Conversion string (char) to float
-                joint3_sp = strtof(char_pt2 + 1, &char_pt3); // Add 1 to bring up the comma
-                // printf("Position to joint1: %.2f and position to joint 2: %.2f \n", joint1_sp, joint2_sp);
+                joint1_sp = strtof(in_buffer, &char_pt1);
                 break;
             }
-            joint_setpoint1 = joint1_sp;
-            joint_setpoint2 = joint2_sp;
-            joint_setpoint3 = -joint3_sp;
             input_char = getchar_timeout_us(0);
-            //printf("\n Caracter recibido \n");
+            joint_setpoint1 = joint1_sp;
         }
-        // gpio_put(LED_PIN, false);
-        // printf("Entradas recibidas");
-        //printf("Effort: J1: %.3f, J2: %.3f, J3: %.3f \n", joint_effort1, joint_effort2, joint_effort3);
-        //printf("Position: J1: %.3f, J2: %.3f \n", joint_position1, joint_position2);
-        //printf("Ef1: %.4f, \n", joint_effort1);
+        float currT = millis() / 1e3 - 3;
+        if (currT < t_final)
+        {
+            joint_setpoint1 = a + b * currT + c * pow(currT, 2) + d * pow(currT, 3);
+            speed_target = b + 2 * c * currT + 3 * d * pow(currT, 2);
+        }
+        else
+        {
+            sleep_ms(500);
+            joint_setpoint1 = 0;
+            speed_target = 0;
+        }
         printf("%.4f, ", joint_position1);
-        printf("%.4f, ", joint_position2);
-        printf("%.4f \n", joint_position3);/*
-        printf("Pos2: %.4f\n \n", joint_position2);*/
-        sleep_ms(10);
+        printf("%.4f, ", joint_setpoint1);
+        printf("%.4f, ", speed_target);
+        printf("%.3f \n", v1Filt);
+        sleep_ms(30);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
     }
 }
