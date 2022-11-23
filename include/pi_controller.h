e922dfa674413a45195b48b8e327015dc947f805
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2022-11-23 10:21:53 -0500

    Update PI controller parameters

--- include/pi_controller.h
+++ include/pi_controller.h
@@ -2,41 +2,38 @@
 // Created by pepe on 21/6/21.
 //
 
-/*#ifndef DIFF_DRIVE_PID_CONTROLLER_H
-#define DIFF_DRIVE_PID_CONTROLLER_H*/
+#ifndef PI_CONTROLLER_H
+#define PI_CONTROLLER_H
+
 #include "pico/stdlib.h"
 #include "dc_motor.h"
 #include "encoder_cuadratura.h"
 #include "cstdio"
 
-class PID {
+class PI
+{
 public:
-    PID(float* input, float* output, float* setpoint, float kp, float ki, float kd, uint32_t sample_time_ms);
+    PID(float *input, float *output, float *setpoint, float kp, float ki, uint32_t sample_time_ms);
     void compute(void);
     void set_output_limits(float min, float max);
-    void set_gains(float kp, float ki, float kd);
+    void set_gains(float kp, float ki);
     void set_sample_time(uint32_t new_sample_time_ms);
 
 private:
     uint32_t _sample_time_ms;
     uint32_t _last_time;
     float _kp;
-    float _kd;
     float _ki;
 
-    float* _my_input;
-    float* _my_output;
-    float* _my_setpoint;
+    float *_my_input;
+    float *_my_output;
+    float *_my_setpoint;
     float _out_min;
     float _out_max;
     float _out_sum;
     float _err_sum;
     float _last_err;
-    float _last_input;
-    float _llast_input;
     float _last_output;
-
 };
 
-
-//#endif //DIFF_DRIVE_PID_CONTROLLER_H
+#endif PI_CONTROLLER_H
--- src/pi_controller.cpp
+++ src/pi_controller.cpp
@@ -4,40 +4,36 @@
 
 #include "pi_controller.h"
 
-PID::PID(float *input, float *output, float *setpoint, float kp, float ki, float kd, uint32_t sample_time_ms)
+PI::PI(float *input, float *output, float *setpoint, float kp, float ki, uint32_t sample_time_ms)
     : _my_input(input), _my_output(output), _my_setpoint(setpoint), _sample_time_ms(sample_time_ms), _out_sum(0), _last_input(0)
 {
     set_output_limits(-1.0f, 1.0f);
-    set_gains(kp, ki, kd);
+    set_gains(kp, ki);
     this->_last_err = 0.0;
     this->_last_output = 0.0;
-    this->_last_input = 0.0;
-    this->_llast_input = 0.0;
 }
 
-void PID::set_sample_time(uint32_t new_sample_time_ms)
+void PI::set_sample_time(uint32_t new_sample_time_ms)
 {
     float ratio = (float)new_sample_time_ms / _sample_time_ms;
 
-    _ki *= ratio;
-    _kd /= ratio;
+    _ki /= ratio;
     _sample_time_ms = new_sample_time_ms;
 }
 
-void PID::set_gains(float kp, float ki, float kd)
+void PI::set_gains(float kp, float ki)
 {
-    if (kp < 0 || ki < 0 || kd < 0)
+    if (kp < 0 || ki < 0)
         return;
 
     float sample_time_s = (float)_sample_time_ms / 1000.0f;
     printf("pid sample time: %f [s]", sample_time_s);
 
     _kp = kp;
-    _ki = ki / _sample_time_ms;  // Se cambió por un intento de discretizado
-    _kd = kd / sample_time_s;
+    _ki = ki / _sample_time_ms; // Se cambió por un intento de discretizado
 }
 
-void PID::set_output_limits(float min, float max)
+void PI::set_output_limits(float min, float max)
 {
     if (min >= max)
         return;
@@ -55,13 +51,11 @@ void PID::set_output_limits(float min, float max)
         _out_sum = _out_min;
 }
 
-void PID::compute(void)
+void PI::compute(void)
 {
     float input = *_my_input;
     float error = *_my_setpoint - input;
 
-    // float output = _last_output + (_kp + _ki + _kd) * error - (_kp + 2.0 * _kd) * _last_err + _kd * _last_err2;
-    // float output = _last_output + error * (_kp * _kd * _sample_time_ms / 1000.0f+ _ki) - _last_err * (_kp * _kd * _sample_time_ms / 1000.0f) - _kd * input + 2 * _kd * _last_input - _kd * _llast_input;
     float output = _last_output + error * (_kp + _ki) - _last_err * _kp;
 
     if (output > _out_max)
@@ -72,6 +66,4 @@ void PID::compute(void)
     *_my_output = output;
     _last_output = output;
     _last_err = error;
-    _llast_input = _last_input;
-    _last_input = input;
 }
