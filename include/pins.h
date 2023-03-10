07cbd8c97f35ee54d36703b763e248ecc822b384
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2023-03-10 18:32:59 -0500

    Update pins after PCB conexion

--- include/pins.h
+++ include/pins.h
@@ -12,18 +12,18 @@
 #define I2C_SCL_PIN 5
 
 /*-------------- MOTOR PINS --------------*/
-#define M0_ENA_PIN 10
-#define M0_ENB_PIN 11
+#define M0_ENA_PIN 15
+#define M0_ENB_PIN 14
 #define M1_ENA_PIN 12
 #define M1_ENB_PIN 13
-#define M2_ENA_PIN 14
-#define M2_ENB_PIN 15
+#define M2_ENA_PIN 11
+#define M2_ENB_PIN 10
 
 /*------------- ENCODER PINS -------------*/
-#define M0_ENC_A_PIN 19 // 21
-#define M0_ENC_B_PIN 18 // 20
-#define M1_ENC_A_PIN 21 // 19
-#define M1_ENC_B_PIN 20 // 18
+#define M0_ENC_A_PIN 21 // 21
+#define M0_ENC_B_PIN 20 // 20
+#define M1_ENC_A_PIN 19 // 19
+#define M1_ENC_B_PIN 18 // 18
 #define M2_ENC_A_PIN 17 // 17
 #define M2_ENC_B_PIN 16 // 16
 
@@ -34,7 +34,7 @@
 
 #define M0_ENC_INVERTED false
 #define M1_ENC_INVERTED true
-#define M2_ENC_INVERTED true
+#define M2_ENC_INVERTED false
 
 #endif
 /*################ GROUP 2 - SLAVE ################*/
@@ -44,12 +44,12 @@
 #define I2C_SDA_PIN 26
 #define I2C_SCL_PIN 27
 /*-------------- MOTOR PINS --------------*/
-#define M3_ENA_PIN 10
-#define M3_ENB_PIN 11
-#define M4_ENA_PIN 12
-#define M4_ENB_PIN 13
-#define M5_ENA_PIN 14
-#define M5_ENB_PIN 15
+#define M3_ENA_PIN 14
+#define M3_ENB_PIN 15
+#define M4_ENA_PIN 13
+#define M4_ENB_PIN 12
+#define M5_ENA_PIN 10
+#define M5_ENB_PIN 11
 
 /*------------- ENCODER PINS -------------*/
 #define M3_ENC_A_PIN 21
@@ -60,12 +60,12 @@
 #define M5_ENC_B_PIN 16
 
 /*------------- SW_HOME PINS -------------*/
-#define M3_HOME_SW 7
+#define M3_HOME_SW 9
 #define M4_HOME_SW 8
-#define M5_HOME_SW 9
+#define M5_HOME_SW 7
 
 #define M3_ENC_INVERTED true
-#define M4_ENC_INVERTED false
+#define M4_ENC_INVERTED true
 #define M5_ENC_INVERTED true
 
 #endif
